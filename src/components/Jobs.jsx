ba360d7393d734c7a054477474c3c4bb8989dfbd
jasonh232 <jason232013@gmail.com>
2024-07-28 02:08:13 -0400

    few changes to Jobs.jsx

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -5,20 +5,21 @@ import Button from '@mui/material/Button';
 import { JobList } from '../assets';
 
 const columns = [
-  { field: 'job_title', headerName: 'Job Title', width: 200 },
-  { field: 'company', headerName: 'Company', width: 200 },
-  { field: 'location', headerName: 'Location', width: 150 },
-  { field: 'role', headerName: 'Role', width: 200 },
-  {
-    field: 'application_link',
-    headerName: 'Application Link',
-    width: 250,
-    renderCell: (params) => (
-      <Button href={params.value} target="_blank" rel="noopener noreferrer">
-        Apply Here
-      </Button>
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'job_title', headerName: 'Job Title', width: 200 },
+    { field: 'company', headerName: 'Company', width: 200 },
+    { field: 'location', headerName: 'Location', width: 150 },
+    { field: 'role', headerName: 'Role', width: 200 },
+    {
+        field: 'application_link',
+        headerName: 'Application Link',
+        width: 250,
+        renderCell: (params) => (
+        <Button href={params.value} target="_blank" rel="noopener noreferrer">
+            Apply Here
+        </Button>
     ),
-  },
+    },
 ];
 
 export default function DataTable() {
