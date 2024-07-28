a96a1a6ef0cec675a270db9d5bb861affd5710d3
jasonh232 <jason232013@gmail.com>
2024-07-27 20:12:56 -0400

    created react project and added constants folder

---
+++ src/constants/index.js
@@ -0,0 +1,44 @@
+export const navLinks = [
+    {
+      id: 'about',
+      title: 'About',
+    },
+    {
+      id: 'projects',
+      title: 'Projects',
+    },
+    {
+      id: 'contact',
+      title: 'Contact',
+    },
+];
+
+const internship = [
+    {
+        url: '',
+    },
+];
+
+const externship =[
+    {
+        url: '',
+    },
+];
+
+const jobs = [
+    {
+        url: '',
+    },
+];
+
+const bootcamp = [
+    {
+        url: '',
+    },
+];
+
+const certificate = [
+    {
+        url: '',
+    },
+];
