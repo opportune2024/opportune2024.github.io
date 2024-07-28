ab36f896501a56d672026975f37d78f4a2f924e1
jasonh232 <jason232013@gmail.com>
2024-07-28 03:05:52 -0400

    updated dependencies

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    open: true,
-    cors: {
-      origin: '*', // Allow all origins
-      methods: '*',
-      allowedHeaders: '*',
-    },
-  },
+  base: "/",
 })
