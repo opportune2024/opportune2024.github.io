42f4b263a0bf9d28d937ac09f2c35ef103b9b978
jasonh232 <jason232013@gmail.com>
2024-07-27 21:16:06 -0400

    added deployment script for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
+  server: {
+    port: 5173,
+    open: true,
+    cors: {
+      origin: '*', // Allow all origins
+      methods: '*',
+      allowedHeaders: '*',
+    },
+  },
 })
