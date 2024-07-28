9f2e78d3001ff91879b87a74570b1bc3c05af826
penguince <xiajieou.yc@gmail.com>
2024-07-27 21:54:42 -0400

    link

--- src/constants/index.js
+++ src/constants/index.js
@@ -15,30 +15,184 @@ export const navLinks = [
 
 const internship = [
     {
-        url: '',
+        url: 'https://careers.jpmorgan.com/us/en/students/programs/software-engineer-summer?search=&tags=location__Americas__UnitedStatesofAmerica',
+    },
+    {
+        url: 'https://www.ixl.com/company/careers?gh_jid=7535510002&utm_source=skillsire&utm_medium=skillsire&utm_campaign=skillsire&source=skillsire&ref=skillsire',
+    },
+    {
+        url: 'https://careers.snowflake.com/us/en/job/SNCOUS7559273002EXTERNALENUS/Data-Scientist-Intern-Fall-2024?utm_source=skillsire&utm_medium=skillsire&utm_campaign=skillsire&source=skillsire&ref=skillsire,
+    },
+    {
+        url: 'https://amazon.jobs/en/jobs/2644301/software-development-engineer-2024-us',
+    },
+    {
+        url: 'https://amazon.jobs/en/jobs/2620668/hardware-development-engineer-fall-internship-2024-us',
+    },
+    {
+        url: 'https://netflix.eightfold.ai/careers?query=intern&location=united%20states&pid=790298013546&domain=netflix.com&sort_by=relevance&triggerGoButton=false&show_multiple=false#apply',
+    },
+    {
+        url: 'https://jobs.careers.microsoft.com/global/en/job/1695161/Research-Intern---AI-Infrastructure',
+    },
+    {
+        url: 'https://jobs.careers.microsoft.com/global/en/job/1695206/Research-Intern---AI-driven-System-and-Database-Design',
+    },
+    {
+        url: 'https://careers.tiktok.com/position/7394666581098039602/detail?utm_source=skillsire&utm_medium=skillsire&utm_campaign=skillsire&source=skillsire&ref=skillsire',
+    },
+    {
+        url: 'https://www.google.com/about/careers/applications/jobs/results/83484176157680326-software-engineering-intern-summer-2025?target_level=INTERN_AND_APPRENTICE',
+    },
+    {
+        url: 'https://www.citadel.com/careers/details/quantitative-research-analyst-2025-intern-us-2/',
+    },
+    {
+        url: 'https://www.citadel.com/careers/details/software-engineer-2025-intern-us-2/',
+    },
+    {
+        url: 'https://www.tesla.com/careers/search/job/internship-android-developer-mobile-software-fall-2024-224329',
+    },
+    {
+        url: 'https://www.tesla.com/careers/search/job/internship-energy-optimization-software-fall-2024-223929',
+    },
+    {
+        url: 'https://www.tesla.com/careers/search/job/internship-embedded-systems-software-engineer-vehicle-firmware-fall-2024-222189',
     },
 ];
 
 const externship =[
     {
-        url: '',
+        url: 'https://www.theforage.com/',
     },
 ];
 
 const jobs = [
     {
-        url: '',
+        url: 'https://www.northropgrumman.com/jobs/Engineering/Software/United-States-of-America/Ohio/Wright-Patterson-AFB/R10164135/software-engineer-aht',
     },
+    {
+        url: 'https://draper.wd5.myworkdayjobs.com/en-US/Draper_Careers/job/MA---Cambridge/Software-Developer_JR000257',
+    },
+    {
+        url: 'https://apply.deloitte.com/careers/InviteToApply?jobId=189788',
+    },
+    {
+        url: 'https://btisolutions.wd12.myworkdayjobs.com/en-US/External/job/San-Diego-CA/Entry-Level-Mobile-Network-Test-Engineer--Entry-Level-_JR100105',
+    },
+    {
+        url: 'https://www.usajobs.gov/job/799910600',
+    },
+    {
+        url: 'https://jobs.baesystems.com/global/en/job/BAE1US102746BREXTERNAL/Principal-Software-Engineer',
+    },
+    {
+        url: 'https://jobs.apple.com/en-us/details/200554884/python-software-development-engineer-apps-services',
+    },
+    {
+        url: 'https://careers-ideagenus.icims.com/jobs/1790/job?mobile=false&width=1671&height=500&bga=true&needsRedirect=false&jan1offset=-300&jun1offset=-240',
+    },
+    {
+        url: 'https://jobs.grainger.com/job/LAKE-FOREST-Infrastructure-Engineer-%28Voice-Engineer%29-IL-60045-5202/1168894100/',
+    },
+    {
+        url: 'https://www.usajobs.gov/job/800592600',
+    },
+    {
+        url: 'https://generalmotors.wd5.myworkdayjobs.com/en-US/Careers_GM/job/Mountain-View-California-United-States-of-America/Principal-Software-Engineer--ADAS-Compute-Platform_JR-202416752',
+    },
+    {
+        url: 'https://jobs.smartrecruiters.com/Square/743999999985141-staff-software-engineer-identity-trust',
+    },
+    {
+        url: 'https://jobs.apple.com/en-us/details/200558385/penetration-tester-retail-engineering',
+    },
+    {
+        url: 'https://careers.boozallen.com/careers/JobDetail?jobId=99788',
+    },
+    {
+        url: 'https://wpengine.wd1.myworkdayjobs.com/en-US/WP_Engine/job/Austin/Associate-Developer-Advocate_JR102241',
+    },
+    {
+        url: 'https://jobs.apple.com/en-us/details/200559155/software-security-engineer',
+    },
+    {
+        url: 'https://jobs.apple.com/en-us/details/200556070/wifi-p2p-software-engineer',
+    },
+    {
+        url: 'https://careers.l3harris.com/job/-/-/4832/67628177504',
+    },
+    {
+        url: 'https://www.almacgroup.com/careers/apply/',
+    },
+    {
+        url: 'https://careers.tiktok.com/position/7393134280438139186/detail?spread=5MWH5CQ',
+    },
+    {
+        url: 'https://www.google.com/about/careers/applications/jobs/results/113362882267095750-security-engineer/?src=Online/LinkedIn/linkedin_us&utm_source=linkedin&utm_medium=jobposting&utm_campaign=contract',
+    },
+    {
+        url: 'https://netflix.eightfold.ai/careers/job/790298014615?domain=netflix.com&utm_source=LinkedIn',
+    },
+    {
+        url: 'https://acsgbl.com/careers/',
+    },
+    {
+        url: 'https://jobs.cisco.com/jobs/ProjectDetail/Cloud-Engineer/1422471?source=LinkedIn',
+    },
+    {
+        url: 'https://careers.snowflake.com/us/en/job/SNCOUS7325931002EXTERNALENUS/Software-Engineer-Intern-Core-Database-Engineering-Fall-2024?utm_medium=phenom-feeds&utm_source=linkedin&gh_src=ed5543a62',
+    },
+    {
+        url: 'https://www.amazon.jobs/en/jobs/2687204/jr-software-development-engineer-sunnyvale?cmpid=SPLICX0248M&ss=paid&utm_campaign=cxro&utm_content=job_posting&utm_medium=social_media&utm_source=linkedin.com',
+    },
+    {
+        url: 'https://careers.ibm.com/job/20646439/full-stack-developer-remote/?codes=SN_LinkedIn&Codes=SN_LinkedIn',
+    },
+    {
+        url: 'https://fa-etbx-saasfaprod1.fa.ocs.oraclecloud.com/hcmUI/CandidateExperience/en/sites/CX_1001/job/18921?bid=11773&tid=x_ded4d38e-4b9e-44c0-bdd2-2045891234db',
+    },
+    {
+        url: 'https://careers.leidos.com/jobs/14681897-junior-cloud-engineer?tm_job=R-00139754&tm_event=view&tm_company=2502&bid=11773',
+    },
+    {
+        url: 'https://fa-etbx-saasfaprod1.fa.ocs.oraclecloud.com/hcmUI/CandidateExperience/en/sites/CX_1001/job/19747?bid=11773&tid=x_f3cf676d-2408-4b12-b34e-fd8c6b684ef8',
+    },
+    {
+        url: 'https://careers.peraton.com/jobs/cloud-software-engineer-lead-associate-laurel-maryland-147606-jobs',
+    },
+    {
+        url: 'https://recruiting.paylocity.com/recruiting/jobs/Details/2570523/Interclypse-Inc/DevOps-Engineer',
+    },
+    {
+        url: 'https://www.honorcare.com/honor-careers/career/?jobID=7553497002',
+    },
+    {
+        url: 'https://jobs.smartrecruiters.com/Square/743999999514596-staff-engineer-frontend-fullstack-public-web',
+    },
+   
 ];
 
 const bootcamp = [
     {
-        url: '',
+        url: 'https://www.theodinproject.com/',
+    },
+    {
+        url: 'https://cs50.harvard.edu/college/2024/fall/',
+    },
+    {
+        url: 'https://www.freecodecamp.org/',
+    },
+    {
+        url: 'https://www.datacamp.com/',
     },
 ];
 
 const certificate = [
     {
-        url: '',
+        url: 'https://www.coursera.org/',
+    },
+    {
+        url: 'https://www.udemy.com/',
     },
 ];
