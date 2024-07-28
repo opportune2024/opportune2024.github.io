6bc2064013807d3ba93d9b1ca5d329b2e861ab5f
jasonh232 <jason232013@gmail.com>
2024-07-28 01:55:12 -0400

    updated jobs.jsx

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
-import JobList from '../assets';
+import * as React from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+import Button from '@mui/material/Button';
 
-const Jobs = () => {
+import { JobList } from '../assets';
+
+const columns = [
+  { field: 'job_title', headerName: 'Job Title', width: 200 },
+  { field: 'company', headerName: 'Company', width: 200 },
+  { field: 'location', headerName: 'Location', width: 150 },
+  { field: 'role', headerName: 'Role', width: 200 },
+  {
+    field: 'application_link',
+    headerName: 'Application Link',
+    width: 250,
+    renderCell: (params) => (
+      <Button href={params.value} target="_blank" rel="noopener noreferrer">
+        Apply Here
+      </Button>
+    ),
+  },
+];
+
+export default function DataTable() {
   return (
-    <div className="">
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={JobList}  // Use the imported JSON data
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+      />
     </div>
   );
-};
-
-export default Jobs;
+}
