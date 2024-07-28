89426f8f019cfdc2bca711fd6c006d53b82cd7a3
opportune2024 <opportune2024@gmail.com>
2024-07-28 02:08:37 -0400

    Update csv2json.py
    add id counter for data table!

--- src/assets/csv2json.py
+++ src/assets/csv2json.py
@@ -5,8 +5,11 @@ def csv_to_json(csv_file_path, json_file_path):
     data = []
     with open(csv_file_path, mode='r', newline='', encoding='utf-8') as csvfile:
         reader = csv.reader(csvfile)
+        next(reader)  # Skip header row if there is one
+        id_counter = 1
         for row in reader:
             job = {
+                "id": id_counter,
                 "job_title": row[0],
                 "company": row[1],
                 "location": row[2],
@@ -14,6 +17,7 @@ def csv_to_json(csv_file_path, json_file_path):
                 "application_link": row[4]
             }
             data.append(job)
+            id_counter += 1
     
     with open(json_file_path, mode='w', encoding='utf-8') as jsonfile:
         json.dump(data, jsonfile, indent=4)
