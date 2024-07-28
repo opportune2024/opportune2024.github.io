b01a06693d616034e740ff533edcdd22dca9b0cc
jasonh232 <jason232013@gmail.com>
2024-07-28 02:57:20 -0400

    updated dependencies

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/opportune2024.github.io",
+  base: "/",
   server: {
     port: 5173,
     open: true,
